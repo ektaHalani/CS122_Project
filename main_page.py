78c93e3178417a5cea4c1888e2fc49a64fe1b8f9
Ekta Halani <80295366+ektaHalani@users.noreply.github.com>
2023-12-05 11:53:00 -0800

    Update main_page.py
    - added about page functionality

--- main_page.py
+++ main_page.py
@@ -21,14 +21,13 @@ def index():
     return render_template('car_price_prediction.html', company='default',
                            car_model='default', year='default',
                            fuel_type='default', companies=companies,
-                           car_models=car_models, years=years, mileage='0',
+                           car_models=car_models, years=years,
                            fuel_types=fuel_types)
 
 
 @app.route('/predict', methods=['POST'])
 @cross_origin()
 def predict():
-
     company = request.form.get('company')
 
     car_model = request.form.get('car_models')
@@ -38,11 +37,18 @@ def predict():
 
     data = np.array([car_model, company, year, driven, fuel_type])
     print(data.shape)
-    prediction = model.predict(pd.DataFrame(columns=['name', 'company', 'year', 'miles_driven', 'fuel_type'],
-                                            data=np.array([car_model, company, year, driven, fuel_type]).reshape(1, 5)))
+    prediction = model.predict(pd.DataFrame(
+        columns=['name', 'company', 'year', 'miles_driven', 'fuel_type'],
+        data=np.array([car_model, company, year, driven, fuel_type]).reshape(1,
+                                                                             5)))
     print(prediction)
 
     return str(np.round(prediction[0], 2))
+
+
+@app.route('/about_page', methods=['GET', 'POST'])
+def about_page():
+    return render_template('about_page.html')
 
 
 if __name__ == '__main__':
